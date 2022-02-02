87be31381db153da484082b8bc5c6da699547373
Leticijak <mnogomijeheavy@gmail.com>
2022-02-02 17:24:31 +0100

    ending-practice

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,30 @@
 import React from "react"
 import { ImLocation2 } from "react-icons/im"
 
-const Card = (props) => {
-  console.log(props)
+const Card = ({
+  imageUrl,
+  location,
+  googleMapsUrl,
+  startDate,
+  endDate,
+  title,
+  description,
+}) => {
   return (
     <div className='card-wrap'>
-      <img src={props.imageUrl} width='300px' alt='' />
+      <img src={imageUrl} width='300px' alt='' />
       <div className='info'>
         <div className='titles'>
           <h4>
-            <ImLocation2 color='red' /> {props.location}
+            <ImLocation2 color='red' /> {location}
           </h4>
-          <a href={props.googleMapsUrl}>View on Google Maps</a>
+          <a href={googleMapsUrl}>View on Google Maps</a>
         </div>
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
         <h4>
-          {props.startDate} - {props.endDate}
+          {startDate} - {endDate}
         </h4>
-        <p>{props.description}</p>
+        <p>{description}</p>
       </div>
     </div>
   )
